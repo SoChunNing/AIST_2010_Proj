b73b545ee325688362afa83aed658de11aff598c
Mouseman0919 <132811160+SoChunNing@users.noreply.github.com>
2024-11-16 00:57:42 +0800

    Update dectect_hand_gesture algorithm
    Now the program have 2 mode: Frequency and discrete midi mode. Press 'm' to change mode

--- detect_hand_gesture.py
+++ detect_hand_gesture.py
@@ -16,11 +16,10 @@ def detect_hand_gesture(mode = 0, midinote = 81):
     mp_hands = mp.solutions.hands
     mp_drawing = mp.solutions.drawing_utils
     init = False
-    current_midi = 0
     # Start capturing from the webcam
     cap = cv2.VideoCapture(1)
     # Initialize the hand tracking model
-    with mp_hands.Hands(max_num_hands=1, min_detection_confidence=0.7, min_tracking_confidence=0.7) as hands:
+    with mp_hands.Hands(max_num_hands=1, min_detection_confidence=0.7, min_tracking_confidence=0.7, static_image_mode=False) as hands:
         while True:  # Keep the loop running until the user decides to exit
             ret, frame = cap.read()
             if not ret:
@@ -33,13 +32,16 @@ def detect_hand_gesture(mode = 0, midinote = 81):
             rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
             results = hands.process(rgb_frame)
 
-            # Wait for the 's' key to capture capture the initial y position of the wrist
-            if cv2.waitKey(1) & 0xFF == ord('s'):
-                wrist_y = wrist.y
-                print("Position reset")
-                init = True
-            # Check if hand landmarks are detected
+            #Display useful messages on cv2 window
+            mode_text = f"Mode: {mode_id[mode]}"
+            cv2.putText(frame, mode_text, (10, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0 , 0), 2)
+                
+            #Press 'm' to change mode
+            if cv2.waitKey(respond_time) & 0xFF == ord('m'):
+                mode = (mode + 1) % 2
+                init = False
 
+            # Check if hand landmarks are detected
             if results.multi_hand_landmarks:
                 for hand_landmarks in results.multi_hand_landmarks:
                     # Draw landmarks on the frame
@@ -57,46 +59,58 @@ def detect_hand_gesture(mode = 0, midinote = 81):
                     gesture_id = model.predict(landmarks)[0]
                     gesture_text = gestures[int(gesture_id)]
                     # Extract landmark positions for gesture recognition
+                    
+                    # Use Middle_FINGER_MCP to record change of y-coordinate of hands
+                    middle_mcp = hand_landmarks.landmark[mp_hands.HandLandmark.MIDDLE_FINGER_MCP]
+                    # ***The coordinates are reverse along x-axis(I don't know why...)
+
+                    ''' ↓ For debug use
                     thumb_tip = hand_landmarks.landmark[mp_hands.HandLandmark.THUMB_TIP]
                     index_tip = hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP]
                     middle_tip = hand_landmarks.landmark[mp_hands.HandLandmark.MIDDLE_FINGER_TIP]
-
-                    ### We use wrist to record change of y-coordinate of our hands
-                    wrist = hand_landmarks.landmark[mp_hands.HandLandmark.WRIST]
-                    # ***The coordinates are reverse along x-axis(I don't know why...)
-                    wrist_coordinates_text = f"Wrist Y: {wrist.y:.4f}"
-                    coordinates_text = f"Thumb Tip Y: {thumb_tip.y:.4f} | Index Tip Y: {index_tip.y:.4f} | Middle Tip Y: {middle_tip.y:.4f}"
-                    if mode == 1:
-                        # Display the gesture name on the frame
-                        cv2.putText(frame, gesture_text, (10, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2)
-                        # Display the y-coordinates below the gesture text
-                        cv2.putText(frame, coordinates_text, (10, 70), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
-                        cv2.putText(frame, wrist_coordinates_text, (10, 90), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
+                    #mcp_coordinates_text = f"Middle MCP Y: {middle_mcp.y:.4f}"
+                    #coordinates_text = f"Thumb Tip Y: {thumb_tip.y:.4f} | Index Tip Y: {index_tip.y:.4f} | Middle Tip Y: {middle_tip.y:.4f}"
+                    # Display the gesture name on the frame
+                    #cv2.putText(frame, gesture_text, (10, 150), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2)
+                    # Display the y-coordinates below the gesture text
+                    #cv2.putText(frame, coordinates_text, (10, 170), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
+                    #cv2.putText(frame, mcp_coordinates_text, (10, 190), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2) '''
                     
-                    midi_text = f"Midi Note Number: {midinote}"
-                    cv2.putText(frame, midi_text, (10, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+                    if mode == 0: #Frequency mode
+                        #Credit to Oscar's code
+                        freq = np.clip(middle_mcp.y * 1980 + 20, 20, 2000)  # Map to desired frequency range
+                        sound_synth(freq, mode)
+                        freq_text = f"Frequency: {freq:.0f}Hz"
+                        cv2.putText(frame, freq_text, (10, 40), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+                    else:
+                        midi_text = f"Midi Note Number: {midinote}"
+                        cv2.putText(frame, midi_text, (10, 40), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+                        ###Perform Sound Synth
 
-                    ###Perform Sound Synth
-                    if init:
-                        #Change the midinote according the change of wrist.y
-                        change_of_y = -(wrist.y - wrist_y)
-                        change_of_midi = int((change_of_y)/midi_interval)
-                        if abs(change_of_midi) > 0:
-                            midinote += change_of_midi
-                            wrist_y = wrist.y
-                        if gesture_text == 'fist':
-                            sound_synth(0) #Stop sound synth when detecting fist
-                        
-                        #Avoid sending same midi note
-                        if current_midi != midinote:
-                            sound_synth(midinote)
-                            current_midi = midinote
+                        # Wait for the 's' key to capture capture the initial y position of the mcp
+                        if cv2.waitKey(respond_time) & 0xFF == ord('s'):
+                            mcp_y = middle_mcp.y
+                            print("Position reset")
+                            init = True 
+
+                        if init:
+                            #Change the midinote according the change of mcp.y
+                            change_of_y = -(middle_mcp.y - mcp_y)
+                            change_of_midi = int((change_of_y)/midi_interval)
+                            if abs(change_of_midi) > 0:
+                                midinote += change_of_midi
+                                mcp_y = middle_mcp.y
+                            if gesture_text == 'fist':
+                                sound_synth(0, mode) #Stop sound synth when detecting fist
+                
+                            if cv2.waitKey(respond_time) & 0xFF == ord(' '):
+                                sound_synth(midinote, mode)
 
             # Show the frame
             cv2.imshow('Hand Gesture Recognition', frame)
                 
             # Wait for the 'q' key to be pressed to exit
-            if cv2.waitKey(1) & 0xFF == ord('q'):
+            if cv2.waitKey(respond_time) & 0xFF == ord('q'):
                 print("Exiting the program...")
                 break
 
@@ -104,4 +118,4 @@ def detect_hand_gesture(mode = 0, midinote = 81):
     cap.release()
     cv2.destroyAllWindows()
 
-detect_hand_gesture(1,81) #0: Normal Mode 1:Debug Mode
+detect_hand_gesture(0,81) #0: Frequency Mode 1: Discrete Midi Note Mode
--- sound_syn.py
+++ sound_syn.py
@@ -1,14 +1,18 @@
 ### Boot the SuperCollier server first before running sound_synth()!!!
 from pythonosc import udp_client
-import time
 
-def sound_synth(midinote):
+
+def sound_synth(midi_freq, mode):
     # Setup the OSC client
     client = udp_client.SimpleUDPClient("127.0.0.1", 57120)
     
     # Send each frequency value with a delay
     
-    print(f"Sending Midinote Number: {midinote} ")
-    client.send_message("/from_python", midinote)
+    print(f"Sending Midinote Number: {midi_freq},  ")
+    client.send_message("/from_python", [midi_freq, mode])
+
+
+
+
 
 
